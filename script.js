834b1ab2c483b9395e04ad07185d7c87303774c1
GragasDeTic <108818425+GragasDeTic@users.noreply.github.com>
2022-07-10 17:41:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ var pecas = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 var correto = array.from({length: 1], () => Math.floor(Math.random() " pecas.length+1));
 //console.log( correto é "+correto);
                                                        
-function novojogo(){
+function novoJogo(){
   for (let k=1; k<=pecas.length; k++){
     const btt = document.createElement("button");
     btt.classlist.add ('botao');
     btt.value = [k];
     btt.innerHTML = [k];
     var pontos =0;
-    btt.addEventListener("clicl", function (){
+    btt.addEventListener("click", function (){
       if(btt.value == correto){
         pontos = pontos;
         contador.innterHTML = pontos;
-        btt.style.background ="green";
+        btt.style.background = "green";
         alert("parabéns! Você encontrou um botão sorteado! Você clicou em "+ pontos +" botões antes de achá-lo!");
         jogo.innerHTML = "";
       }else{
